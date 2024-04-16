05d6959cd694d098abf1263c5405500924ac8603
Danny Byrne <danny.byrne.dev@gmail.com>
2024-04-16 15:04:51 -0700

    sort out path import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@src": resolve(__dirname, "src/components"),
+      // '@styles': resolve(__dirname, 'src/styles'),
+      // Add more aliases as needed
+    },
+  },
+});
